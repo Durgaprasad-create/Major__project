6a26f642cd993e76244d0a4b2f9b8faadf9eb33d
Durga prasad <rgvrkvsp8@gmail.com>
2025-04-28 13:54:58 +0530

    Updated admin.py to register models to display Users and Applicants on Amin page

--- Job_Adv/job_adver/job_app/admin.py
+++ Job_Adv/job_adver/job_app/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import Job
+from .models import Job , Profile , JobApplication
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ('JobRole', 'company', 'location', 'category', 'posted_by', 'posted_on')
     search_fields = ('JobRole', 'company', 'location', 'category')
     list_filter = ('category', 'posted_on')
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_type')
+    search_fields = ('user__username',)
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ('job', 'applicant', 'applied_on')
+    search_fields = ('job__JobRole', 'applicant__username')
